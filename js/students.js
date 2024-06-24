bd002515b2885a65cb1f6d5113ebe736d413a4a2
Pascal512 <pascaldegla512@gmail.com>
2024-06-24 21:24:20 +0100

    Form ajout manuel étudiant

--- js/students.js
+++ js/students.js
@@ -19,3 +19,18 @@ formNewStudent.addEventListener('keydown', preventEnterSubmit);
 
 const tab_add = document.getElementById('tab-add-student');
 const tab_import = document.getElementById('tab-import-student');
+const form_new = document.getElementById('form-new-student');
+const importation = document.getElementById('importation');
+tab_add.addEventListener('click', function (){
+    form_new.style.display = "flex";
+    importation.style.display = "none";
+    tab_add.classList.add('modal-tab-element-active');
+    tab_import.classList.remove('modal-tab-element-active');
+});
+tab_import.addEventListener('click', function (){
+    form_new.style.display = "none";
+    importation.style.display = "block";
+    tab_add.classList.remove('modal-tab-element-active');
+    tab_import.classList.add('modal-tab-element-active');
+});
+
